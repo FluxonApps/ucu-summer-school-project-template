251f567b6fe4116139cf5c944fd0064fd45f63b7
Marina Skripnik <marina@fluxon.com>
2024-05-21 18:51:34 +0300

    Integrate Chakra UI, main page updates (#1)

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,16 @@
+import { Box, Img, Link, Stack, Text } from '@chakra-ui/react';
+
+import fluxonLogo from './assets/fluxon-logo.svg';
+
 export const App = () => {
-  return <div>UCU x Fluxon Product Development Bootcamp</div>;
+  return (
+    <Box h="full" bg="radial-gradient(at left top, #050311 20%, #2A53C7 100%)">
+      <Stack spacing={4} justifyContent="center" alignItems="center" h="full">
+        <Link target="_blank" href="https://fluxon.com">
+          <Img w={300} src={fluxonLogo} />
+        </Link>
+        <Text color="white">UCU x Fluxon Product Development Bootcamp</Text>
+      </Stack>
+    </Box>
+  );
 };
