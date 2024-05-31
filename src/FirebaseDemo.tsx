2ef7d3a69a2ebb42b58fd136b219046f03cfebc0
Nazar <nazar@fluxon.com>
2024-05-31 17:06:08 +0300

    Add react-firebase-hooks

--- src/FirebaseDemo.tsx
+++ src/FirebaseDemo.tsx
@@ -1,41 +1,45 @@
-import { useState, useEffect } from 'react';
-import { db } from '../firebase.config';
-import { collection, getDocs, addDoc, updateDoc, deleteDoc, doc, QuerySnapshot } from 'firebase/firestore';
-import { Box, Button, Flex, Heading, Input, Stack, HStack } from '@chakra-ui/react';
+import { Box, Button, Flex, Heading, Input, Stack, HStack, Spinner } from '@chakra-ui/react';
+import { collection, addDoc, updateDoc, deleteDoc, doc, query, CollectionReference } from 'firebase/firestore';
+import { useState } from 'react';
+import { useCollection } from 'react-firebase-hooks/firestore';
 
+import { db } from '../firebase.config';
+
+interface User {
+  id: string;
+  name: string;
+  mark: number;
+}
 export function FirebaseDemo() {
   const [newName, setNewName] = useState('');
   const [newMark, setNewMark] = useState(0);
 
-  const [users, setUsers] = useState<Array<{ id: string; name: string; mark: number }>>([]);
   const usersCollectionRef = collection(db, 'users');
+
+  const [users, usersLoading, usersError] = useCollection(query(usersCollectionRef) as CollectionReference<User>);
 
   const createUser = async () => {
     await addDoc(usersCollectionRef, { name: newName, mark: Number(newMark) });
-    getUsers();
   };
 
   const updateUser = async (id: string, mark: number) => {
     const userDoc = doc(db, 'users', id);
     const newFields = { mark: mark + 1 };
     await updateDoc(userDoc, newFields);
-    getUsers();
   };
 
   const deleteUser = async (id: string) => {
     const userDoc = doc(db, 'users', id);
     await deleteDoc(userDoc);
-    getUsers();
   };
 
-  const getUsers = async () => {
-    const data = (await getDocs(usersCollectionRef)) as QuerySnapshot<{ id: string; name: string; mark: number }>;
-    setUsers(data.docs.map((doc) => ({ id: doc.id, name: doc.data().name, mark: doc.data().mark })));
-  };
+  if (usersLoading) {
+    return <Spinner />;
+  }
 
-  useEffect(() => {
-    getUsers();
-  }, []);
+  if (usersError) {
+    return <Box>Error fetching users</Box>;
+  }
 
   return (
     <Flex flexDir="column" gap="8" padding="6">
@@ -62,20 +66,21 @@ export function FirebaseDemo() {
         </Button>
       </Flex>
       <Flex gap="4" flexWrap="wrap">
-        {users.map((user) => (
-          <Box gap="6" border="1px" borderColor="gray.300" width="20%" px="6" py="8" key={user.id}>
-            <Heading>Name: {user.name}</Heading>
-            <Heading>Mark: {user.mark}</Heading>
-            <HStack gap="4" mt="4">
-              <Button size="sm" colorScheme="green" onClick={() => updateUser(user.id, user.mark)}>
-                Increase Mark
-              </Button>
-              <Button size="sm" colorScheme="red" onClick={() => deleteUser(user.id)}>
-                Delete User
-              </Button>
-            </HStack>
-          </Box>
-        ))}
+        {users &&
+          users.docs.map((user) => (
+            <Box gap="6" border="1px" borderColor="gray.300" width="20%" px="6" py="8" key={user.id}>
+              <Heading>Name: {user.data().name}</Heading>
+              <Heading>Mark: {user.data().mark}</Heading>
+              <HStack gap="4" mt="4">
+                <Button size="sm" colorScheme="green" onClick={() => updateUser(user.id, user.data().mark)}>
+                  Increase Mark
+                </Button>
+                <Button size="sm" colorScheme="red" onClick={() => deleteUser(user.id)}>
+                  Delete User
+                </Button>
+              </HStack>
+            </Box>
+          ))}
       </Flex>
     </Flex>
   );
